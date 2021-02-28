6d84d5de11a0f387a03f3c73f7d7deb1e5e2b22d
ashish singh <ashish.singh@uto.in>
2021-03-01 00:37:28 +0530

    set to local

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -3,7 +3,6 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  // const fullPath = path.join(__dirname + '../dist/bse-sensex/index.html');
   const rootFolder = "/bse-sensex";
   const fullPath = __dirname.substring(0, __dirname.indexOf(rootFolder) + rootFolder.length);
   const distPath = path.join(fullPath + "/dist/bse-sensex/index.html");
