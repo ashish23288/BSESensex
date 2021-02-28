020264e2e7e8964777aedd64c43e5ef71018431a
ashish singh <ashish.singh@uto.in>
2021-02-28 20:42:10 +0530

    removed server

--- server/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const stockController = require("../controllers/Sensex");
-
-router.get("/:pageNo", stockController.getAllStockes);
-
-router.post("/addStock", stockController.addStock);
-
-module.exports = router;
--- server/validations/addStockSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-const addStockSchema = {
-  open: Joi.number().min(0).required(),
-  close: Joi.number().min(0).required()
-};
-
-module.exports = addStockSchema;
