f0387f109a25e7ef7858e5181d5c1aac8653dd4c
ashish singh <ashish.singh@uto.in>
2021-02-28 12:32:04 +0530

    code removed

--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -11,10 +11,8 @@ import { StockApiData } from '../modals/stock';
 export class StockService {
   socket;
   constructor(private http: HttpClient) {
-    this.socket = io('http://localhost:3000', { transports: ['websocket'] });
-    console.log(this.socket);
-
-   }
+    this.socket = io(environment.serverURL, { transports: ['websocket'] });
+  }
 
   getAllStockes(page = 1) {
     return this.http.get<StockApiData>(`${environment.serverURL}/${page}`);
